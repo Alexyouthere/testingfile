9e3c059082de6b0409f98c5552d4d6c60e9a8674
Alexyouthere <121602471+Alexyouthere@users.noreply.github.com>
2024-09-07 22:16:40 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,6 @@ import react from '@vitejs/plugin-react'
 
 import { defineConfig } from 'vite'
 
-export default defineConfig({
-  root: process.cwd(),  // or the appropriate root directory
-  // other configuration options...
-})
 
 export default defineConfig({
   build: {
