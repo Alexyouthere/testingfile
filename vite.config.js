eec298caff4a7b616febd1fcf45151a7b32e13ff
Alexyouthere <121602471+Alexyouthere@users.noreply.github.com>
2024-09-07 22:19:05 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-import { defineConfig } from 'vite'
-
-
 export default defineConfig({
+  plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['/src/main.jsx']
+      input: '/src/main.jsx', // Specify the entry point
+    }
+  },
+  resolve: {
+    alias: {
+      '/src': '/src' // This helps ensure proper resolution of /src paths
     }
   }
 })
